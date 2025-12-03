18297208ca1bb30807429fb98427b0a3e53c5684
Helvetic <160547598+HelveticZiel@users.noreply.github.com>
2025-12-03 11:25:46 +0100

    Basic structure Model

---
+++ MVC_ejemplo/Model/userModel.php
@@ -0,0 +1,22 @@
+<?php
+
+class PlantaModel {
+    public function obtenerPlantas(){
+
+        //Idealmente vendrían de una base de datos preexistente
+
+        return [
+            ["nombre" => "Roble", "tipo" => "Árbol"],
+            ["nombre" => "Lavanda", "tipo" => "Planta"],
+            ["nombre" => "Abedul", "tipo" => "Árbol"],
+            ["nombre" => "Aloe Vera", "tipo" => "Planta"]
+        ];
+
+    }
+}
+
+
+
+
+
+?>
