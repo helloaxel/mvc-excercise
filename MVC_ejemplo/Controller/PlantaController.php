0700bba70f0a2089a7d510a9333edeb02c91f536
Helvetic <160547598+HelveticZiel@users.noreply.github.com>
2025-12-03 11:33:17 +0100

    Rename files + define controller structure

---
+++ MVC_ejemplo/Controller/PlantaController.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/../Model/PlantaModel.php';
+
+class PlantaController {
+    private $model;
+
+    public function __construct() {
+        $this->model = new PlantaModel();
+    }
+
+    public function mostrarLista() {
+        $plantas = $this->model->obtenerPlantas();
+        include __DIR__ . '/../View/lista_plantas.php';
+    }
+
+}
+
+
+?>
